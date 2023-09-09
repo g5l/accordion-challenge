6c8d5da43a1437adc76de9b593b1ee84af594ac8
Gabriel Debona <gabriel.debona@gmail.com>
2023-09-09 20:05:24 -0300

    feat: Added support to styled-components

---
+++ src/app/styles.tsx
@@ -0,0 +1,11 @@
+'use client'
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  background: #EEEEEE;
+`
