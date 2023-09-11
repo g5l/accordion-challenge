098150c5b4563987d6964083c6b8f8d3251bb2a5
Gabriel Debona <gabriel.debona@gmail.com>
2023-09-10 21:37:08 -0300

    feat: Updated the ProgressBar component to improve the transition UX

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,10 +1,12 @@
-import * as React from 'react'
+import React from 'react'
 import {Props} from './types';
 import {Bar, Progress} from './styles'
 
 const ProgressBar: React.FC<Props> = (props) => (
   <Bar>
-    <Progress percentage={props.percentage}>{props.percentage}%</Progress>
+    <Progress percentage={props.percentage}>
+      {props.percentage}%
+    </Progress>
   </Bar>
 )
 
--- src/components/ProgressBar/styles.tsx
+++ src/components/ProgressBar/styles.tsx
@@ -13,12 +13,13 @@ export const Progress = styled.div<Props>`
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  width: ${(props) => props.percentage}%;
+  width: ${(props) => props.percentage !== 0 ? `${props.percentage}%` : "fit-content"};
   height: 100%;
+  color: ${(props) => props.percentage == 0 ? "#00B797" : "#FFFFFF"};
   padding: 0 16px;
   border-radius: 24px;
-  background: #00B797;
+  background: ${(props) => props.percentage === 0 ? "transparent" : "#00B797"};
   font-size: 16px;
   font-weight: 600;
-  transition: width 0.5s ease;
+  transition: width 0.5s ease, padding 0.5s ease;
 `
