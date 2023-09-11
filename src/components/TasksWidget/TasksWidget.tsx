e71e919779f39dd02587c15cc35fd9b727de311d
Gabriel Debona <gabriel.debona@gmail.com>
2023-09-10 21:38:49 -0300

    feat: Updated TasksWidget to change the tasksGroups whe it's updated

--- src/components/TasksWidget/TasksWidget.tsx
+++ src/components/TasksWidget/TasksWidget.tsx
@@ -1,18 +1,61 @@
-import * as React from 'react'
+'use client'
+import React from 'react'
 import {Container, Header, Title} from './styles'
 import ProgressBar from "@/components/ProgressBar/ProgressBar";
+import {ProfileTask, ProfileTasksGroup} from "@/types/profile";
+import AccordionGroup from "@/components/AccordionGroup";
+import Accordion from "@/components/Accordion";
+import Checkbox from "@/components/Checkbox";
+import {calculateTasksPercentage} from "@/service/profile";
+import {CheckEvent} from "@/types/common";
 
 type Props = {
-  title: string
+  title: string,
+  tasksGroups: ProfileTasksGroup[],
+  onChange: (profileTasksGroup: ProfileTasksGroup[]) => void
 }
 
-const TasksWidget: React.FC<Props> = (props) => (
-  <Container>
-    <Header>
-      <Title>{props.title}</Title>
-      <ProgressBar percentage={50}/>
-    </Header>
-  </Container>
-)
+const TasksWidget: React.FC<Props> = (props) => {
+
+  const handleCheckboxChange = (event: CheckEvent) => {
+    const updatedTasksGroups = props.tasksGroups.map(group => ({
+      ...group,
+      tasks: group.tasks.map(task => task.id === event.id ? {...task, checked: event.checked} : task)
+    }));
+    props.onChange(updatedTasksGroups);
+  }
+
+  const displayGroups = () => {
+    return props.tasksGroups.map((tasksGroup, index) => (
+      <Accordion key={index} title={tasksGroup.name}>
+        {displayTasks(tasksGroup.tasks)}
+      </Accordion>
+    ))
+  }
+
+  const displayTasks = (tasks: ProfileTask[]) => {
+    return tasks.map((task, index) => (
+      <Checkbox
+        key={index}
+        id={task.id}
+        label={task.description}
+        checked={task.checked}
+        onChange={handleCheckboxChange}
+      />
+    ))
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Title>{props.title}</Title>
+        <ProgressBar percentage={calculateTasksPercentage(props.tasksGroups)}/>
+        <AccordionGroup>
+          {displayGroups()}
+        </AccordionGroup>
+      </Header>
+    </Container>
+  )
+}
 
 export default TasksWidget;
