0b104c0c3c98e2ae3ac4847db2284881eede4e8a
Gabriel Debona <gabriel.debona@gmail.com>
2023-09-12 22:36:46 -0300

    feat: Added global style types and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+'use client'
+const theme = {
+  greyscale900: '#333333',
+};
+
+export default theme;
---
+++ src/types/styled.d.ts
@@ -0,0 +1,8 @@
+'use client'
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    greyscale900: string;
+  }
+}
